d850498d9fa0eac8abee0249e0d93395abb3e00b
cchave17 <carloschavezdeveloper@gmail.com>
2022-06-27 23:06:50 -0500

    liabilities into objects

--- py_accountant/csv_utils.py
+++ py_accountant/csv_utils.py
@@ -1,20 +1,27 @@
 import csv
 
+from py_accountant.modules.expense import Expense
 
 LIABILITIES = './../data/Liabilities.csv'
 EXPENSES = './../data/Liabilities.csv'
 
 
 def get_csv_file_data(FILEPATH):
+    """
 
-    amount = 0
+    """
+    expenses = []
 
     with open(FILEPATH, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            amount += float(row["Amount"])
-            # recurring_date = row['Date']
-    print(amount)
+            recurring_date = row['Date']
+            name = row["Name"]
+            amount= row["Amount"]
+            cur_exp = Expense(recurring_date,name,amount)
+            expenses.append(cur_exp)
+
+
 
 get_csv_file_data(EXPENSES)
 
